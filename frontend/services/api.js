0c86777ac20da7c98774a33cdb6e47a92be83fb8
suyog-2005 <suyogkvrwf2020.2022@gmail.com>
2026-01-22 20:06:24 +0530

    second commit

--- frontend/services/api.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const API = axios.create({ baseURL: 'http://localhost:5000' });
-
-export const login = (data) => API.post('/auth/login', data);
-export const markAttendance = (subjectId, studentId) =>
-  API.post('/attendance', { subjectId, studentId });
